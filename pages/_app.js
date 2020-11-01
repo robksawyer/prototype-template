6da49c7d1418de03bc2b9994012c1b602d8f7ae7
Rob Sawyer <rob@layerframe.com>
2020-11-01 17:44:44 +0900

    Added some styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,15 @@
+// Import Font Styles
+// You can find fonts at https://fonts.adobe.com/
+// @import url('https://use.typekit.net/tyl1imq.css');
+import '../styles/fonts.css'
+
+// Add a custom scrollbar
+// import '../styles/custom-scrollbar.css'
+
+// Import Tailwind Styles
+import '../styles/tailwind.css'
+import '../styles/tailwind-custom.css'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
