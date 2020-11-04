2a9c0aa4ee7b15e32fb13bc3c1c479ad3b4bee19
Rob Sawyer <rob@layerframe.com>
2020-11-04 23:39:20 +0900

    Added loader for glsl shaders. Updated the body font size

--- next.config.js
+++ next.config.js
@@ -85,6 +85,13 @@ const nextConfig = {
           loader: 'eslint-loader',
         }
       )
+
+      // Load GLSL Shaders
+      config.module.rules.push({
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader', 'glslify-loader'],
+      })
     }
     return config
   },
