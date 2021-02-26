c252c6f634ddc6b1575345b4d6a58735a6d3f623
Rob Sawyer <rob@layerframe.com>
2021-02-26 18:14:58 +0900

    Updated for build

--- next.config.js
+++ next.config.js
@@ -86,14 +86,14 @@ const nextConfig = {
           loader: 'eslint-loader',
         }
       )
-
-      // Load GLSL Shaders
-      config.module.rules.push({
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ['raw-loader', 'glslify-loader'],
-      })
     }
+
+    // Load GLSL Shaders
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    })
     return config
   },
   env: {},
