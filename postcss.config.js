82a4947a7c5ea6f1211a37e0526d4bf2fbe0ca48
Rob Sawyer <rob@layerframe.com>
2021-03-18 18:33:31 +0900

    Bug fix

--- postcss.config.js
+++ postcss.config.js
@@ -13,6 +13,9 @@ module.exports = {
         stage: 1,
         features: {
           'nesting-rules': true,
+
+          // Fixes: https://github.com/tailwindcss/tailwindcss/issues/1190#issuecomment-546621554
+          'focus-within-pseudo-class': false,
         },
         // autoprefixer: {
         //   grid: true
